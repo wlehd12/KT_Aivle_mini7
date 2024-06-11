83d4f424bed49b2b63f851ea8f79ab10bf0fb837
ronnie1999 <ronnie1999@naver.com>
2024-06-11 15:59:15 +0900

    Revert "merge test"
    This reverts commit 0967e2abb4fbb6f6933287a49ba8e9c2f2c43601.

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,6 @@ def index(request):
 urlpatterns = [
     path('',index),
     path('chatgpt/',include('chatgpt.urls')),
-    path('selfchatgpt/', include('selfchatgpt.urls')),
     path('admin/', admin.site.urls),
 ]
 
