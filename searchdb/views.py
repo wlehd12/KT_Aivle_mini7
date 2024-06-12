1a74500e74a9364e412db953bbef96595aa025f5
jusebi <sweetapple923@gmail.com>
2024-06-12 13:32:29 +0900

    search html changed

--- searchdb/views.py
+++ searchdb/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import ChatgptHelpaivleqa
 
 def index(request):
     return render(request, 'search/index.html')
@@ -7,3 +7,10 @@ def index(request):
 # Create your views here.
 def searchDB(request):
     return request
+
+def list(request):
+    QAlist = ChatgptHelpaivleqa.objects.all() 
+    search_key = request.GET.get("keyword")
+    if search_key:
+        QAlist = ChatgptHelpaivleqa.objects.filter(title__icontains=search_key)
+    return render(request, 'blog/list.html', {'post_all':QAlist, 'q':search_key})
