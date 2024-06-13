ee373a864a88a8ae276d9512f8a6cd8ca1b21dda
yckim <95165014+0000ch@users.noreply.github.com>
2024-06-13 10:40:42 +0900

    join

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login, authenticate
 from django.conf import settings
 
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect(settings.LOGIN_REDIRECT_URL)
     else:
         form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form':form})
+    return render(request, 'registration/signup.html', {'form': form})
